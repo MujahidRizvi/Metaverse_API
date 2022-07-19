65b231a0622e6a255b6aac1931c458e1b5f7d0c1
MujahidRizvi <superizvi@gmail.com>
2022-07-19 18:33:36 +0500

    commit2

--- cypress/e2e/all_assets.cy.ts
+++ cypress/e2e/all_assets.cy.ts
@@ -4,7 +4,9 @@ describe('All assests', () => {
       cy.request({
         method: 'GET', url: 'https://staging-gateway.wrld.xyz/assets', headers: { "Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0" }, failOnStatusCode: false
       }).then(
-        (response) => {
+        (response) => {  
+
+          
           //This line will verify th inputs property i.e.  plus the value of internal type i.e. unit256
           expect(response.body.data).to.have.property("result")
           expect(response.body.data).to.have.property("page")
