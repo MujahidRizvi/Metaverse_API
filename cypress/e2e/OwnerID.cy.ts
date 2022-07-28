66cac3fd73af28a0bd2f2d8691b9f61fdb5ec86b
MujahidRizvi <superizvi@gmail.com>
2022-07-28 18:06:01 +0500

    Assets by OwnerID

--- cypress/e2e/OwnerID.cy.ts
+++ cypress/e2e/OwnerID.cy.ts
@@ -1,33 +1,34 @@
-  describe('Get Asset by Owner ID', () => {
-      context('GET /Owner ID', () => {
-        it('should show Asset by Owner ID', () => {
+describe('Automation Test Suite - Fixtures', function () {
+  context('get /All Assets by OwnerID', () => {
+    this.beforeEach(function () {
+      cy.fixture('data').then(function (testdata) {
+        this.testdata = testdata;
+      })
+    })
+        it('should show Asset by Owner ID', function()  {
           cy.request({
-            method: 'GET', url: 'https://staging-gateway.wrld.xyz/assets/getAssetsByOwner/:id', headers: { "Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E2XYy0" }, failOnStatusCode: false
+            method: this.testdata.get, url: this.testdata.AssetbyOwner, headers: { "Cookie": this.testdata.tokenA }, failOnStatusCode: false
           }).then(
             (response) => {
-            //This line will verify th inputs property i.e. internalType plus the value of internal type i.e. unit256
-              expect(response.body.data).to.have.property("result")
-              expect(response.body.data).to.have.property("total")
-              expect(response.body.data).to.have.property("page")
-              expect(response.body.data).to.have.property("totalPages")
+              expect(response.body.data).to.have.keys(this.testdata.OwnerIdKeys)
             
             })
-        });
       })
       {
-        it('should not show Asset by Owner ID', () => {
-        cy.request({method: 'GET', url:'https://staging-gateway.wrld.xyz/assets/getAssetsByOwner/:id', headers: {}, failOnStatusCode: false}).then(
+        it('should not show Asset by Owner ID', function()  {
+        cy.request({method: this.testdata.get, url: this.testdata.AssetbyOwner, headers: {}, failOnStatusCode: false}).then(
           (response) => {
-            expect(response.body).contain('Unauthorized') 
+            expect(response.body).contain(this.testdata.uAuth) 
           })
 
-          cy.request({method: 'GET', url:'https://staging-gateway.wrld.xyz/assets/getAssetsByOwner/:id', headers: {"Cookie": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1Mjc5MDI1NCwiZXhwIjoxNjg0MzI2MjU0fQ.2vIFGELbH54q12-b1SIMwm0Eh7BLmdJMpPEX3E21234"}, failOnStatusCode: false}).then(
+          cy.request({method: this.testdata.get, url:this.testdata.AssetbyOwner, headers: {"Cookie": this.testdata.invalidToken}, failOnStatusCode: false}).then(
           (response) => {
-            expect(response.body).contain('Unauthorized') 
+            expect(response.body).contain(this.testdata.uAuth) 
           })
     })
     }
     })
+  })
     
     
     
