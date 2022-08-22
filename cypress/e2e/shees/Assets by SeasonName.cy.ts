9f7e50d874a500079ddf3e2464a014cd4f7df045
MujahidRizvi <superizvi@gmail.com>
2022-08-22 21:30:25 +0500

    Commit 4

--- cypress/e2e/shees/Assets by SeasonName.cy.ts
+++ cypress/e2e/shees/Assets by SeasonName.cy.ts
@@ -1,23 +1,24 @@
 describe('Automation Test Suite - Fixtures', function () {
-  context('get /All SeasonName', () => {
+  context('get /All Assets by SeasonName', () => {
     this.beforeEach(function () {
       cy.fixture('data').then(function (testdata) {
         this.testdata = testdata;
       })
     })
 
-    it('should show Asset by Season Name', function() {
+    it('should show Asset by Season Name', function () {
       cy.request({
         method: this.testdata.get, url: this.testdata.AssetbySeason, headers: { "Cookie": this.testdata.tokenA }, failOnStatusCode: false
       }).then(
         (response) => {
-          expect(response.body.data[0]).to.have.keys(this.testdata.DataforTypeorSeasonKeys)
+          cy.wait(12000)
+          expect(response.body.data[0]).to.contain.keys(this.testdata.DataforTypeorSeasonKeys)
 
         })
     })
 
     {
-      it('should not show Asset by Season Name', function() {
+      it('should not show Asset by Season Name', function () {
         cy.request({ method: this.testdata.get, url: this.testdata.AssetbySeason, headers: {}, failOnStatusCode: false }).then(
           (response) => {
             expect(response.body).contain(this.testdata.uAuth)
